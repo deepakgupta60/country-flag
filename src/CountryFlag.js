de2a195e1d36264c362c2d870725e8637614712a
Deepak Gupta <deepakgupta60k@gmail.com>
2024-10-01 23:35:58 +0530

    working

---
+++ src/CountryFlag.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+
+const CountryFlag = () => {
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let response = await fetch("https://xcountries-backend.azurewebsites.net/all");
+            let data = await response.json();
+            setCountry(data)
+        }
+        fetchData()
+    }, [])
+
+    const CountryData = ({ name, flag, abbr }) => {
+
+        return (
+            <>
+                <div style={{
+
+                    height: "200px",
+                    width: "200px",
+                    border: "0.5px solid",
+                    padding: "10px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius:"5px",
+                    margin: "10px"
+
+                }}>
+                    <img src={flag} height={100} width={100} alt='text' />
+                    <p>{name}</p>
+                </div>
+            </>
+        )
+
+    }
+    return (
+        <>
+
+            <div style={{ display: "flex", flexWrap: "wrap", margin: "auto", width: "1920px" }}>
+
+                {
+                    country.map(({ name, flag, abbr }, index) => (
+                        <div key={index} >
+
+                            <CountryData name={name} flag={flag} />
+                        </div>
+
+
+                    ))
+                }
+
+            </div>
+        </>
+    )
+}
+
+export default CountryFlag
